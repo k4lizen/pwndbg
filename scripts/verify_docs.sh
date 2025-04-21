6e250c2f5bf8444f2e3203662b3d1221962d6bd7
k4lizen <124312252+k4lizen@users.noreply.github.com>
2025-04-21 21:46:42 +0200

    repro

--- scripts/verify_docs.sh
+++ scripts/verify_docs.sh
@@ -5,5 +5,5 @@ export PWNDBG_GEN_DOC_JUST_VERIFY=1
 # Run the verifier inside gdb so everything resolves correctly.
 uv run --group docs gdb --batch -nx --ex "source ./gdbinit.py" \
     --ex "set exception-verbose on" \
+    --ex "maintenance set internal-error corefile yes" \
     --ex "source ./scripts/_gen_function_docs.py" \
-    --quiet
