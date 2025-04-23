9491454b9b905dc810d0d080f641bd953393bf99
k4lizen <124312252+k4lizen@users.noreply.github.com>
2025-04-23 21:30:43 +0200

    bisecting

--- scripts/verify_docs.sh
+++ scripts/verify_docs.sh
@@ -7,17 +7,29 @@ export PWNDBG_GEN_DOC_JUST_VERIFY=1
 #     --ex "source ./gdbinit.py" \
 #     --ex "set exception-verbose on" \
 #     --ex "maintenance set internal-error corefile yes" \
-#     --ex "source ./scripts/_gen_function_docs.py" \
+#     --ex "source ./thing.py"
+#     # --ex "source ./scripts/_gen_function_docs.py" \
 
 
-uv run --group docs ~/opt/gdb-build/gdb/gdb \
-    --data-dir=/home/$USER/opt/gdb-build/gdb/data-directory/ -nx \
-    --ex "source ./gdbinit.py" \
-    --ex "maintenance set internal-error corefile yes" \
-    # --ex "source ./thing.py"
+
+# uv run --group docs ~/opt/gdb-build-15.1/gdb/gdb \
+#     --data-dir=/home/$USER/opt/gdb-build-15.1/gdb/data-directory/ -nx \
+#     --ex "source ./gdbinit.py" \
+#     --ex "set exception-verbose on" \
+#     --ex "maintenance set internal-error corefile yes" \
+#     # --ex "source ./thing.py"
 
 # uv run --group docs ~/opt/gdb-build-master/gdb/gdb \
 #     --data-dir=/home/$USER/opt/gdb-build-master/gdb/data-directory/ -nx \
 #     --ex "source ./gdbinit.py" \
+#     --ex "set exception-verbose on" \
 #     --ex "maintenance set internal-error corefile yes" \
 #     # --ex "source ./thing.py"
+
+# for bisect
+uv run --group docs ~/opt/gdb-build-bisect/gdb/gdb \
+    --data-dir=/home/$USER/opt/gdb-build-bisect/gdb/data-directory/ -nx \
+    --ex "source ./gdbinit.py" \
+    --ex "set exception-verbose on" \
+    --ex "maintenance set internal-error corefile yes" \
+    --ex "source ./thing.py"
